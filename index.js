349f128182c8f461665fa05843a41acd6d3b76ae
Zaid Khan <zaiddkhhan@gmail.com>
2024-03-29 00:33:44 +0530

    custom error

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import ApiRoutes from './routes/api.js'
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({
+    extended : false
+}))
+app.use('/api',ApiRoutes)
+
 const port = process.env.PORT || 8000
 app.get("/",(req,res) => {
     return res.json(
