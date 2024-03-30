29d7a03c5bbfb2b00f6954475901c8e0230f15c2
Zaid Khan <zaiddkhhan@gmail.com>
2024-03-30 19:11:55 +0530

    news api completed

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import ApiRoutes from './routes/api.js'
+import fileUpload from "express-fileupload";
+
+
 const app = express();
+app.use(express.static("public"));
 app.use(express.json())
-app.use(express.urlencoded({
-    extended : false
-}))
+app.use(express.urlencoded({extended : false}))
+app.use(fileUpload())
 app.use('/api',ApiRoutes)
 
 const port = process.env.PORT || 8000
