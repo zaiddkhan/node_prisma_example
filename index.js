b019fa1389a48861d701571ae7a594c334bd2adf
Zaid Khan <zaiddkhhan@gmail.com>
2024-03-31 00:04:57 +0530

    redis cache implemented

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import express from "express";
 import "dotenv/config";
 import ApiRoutes from './routes/api.js'
 import fileUpload from "express-fileupload";
+import helmet from "helmet";
+import cors from "cors";
+import { limiter } from "./config/ratelimiter.js";
 
 
 const app = express();
+app.use(helmet());
+app.use(limiter);
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
